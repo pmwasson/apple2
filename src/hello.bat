77484e0622e11ca75f22ddf3a3cd6988eaab68f8
pmwasson <pmwasson@gmail.com>
2020-12-21 12:59:42 -0800

    clean up build

--- src/hello.bat
+++ src/hello.bat
@@ -1,8 +1,10 @@
 cd ..\build
-ca65 -I ..\src -t apple2 ..\src\hello.asm -l hello.txt
+ca65 -I ..\src -t apple2 ..\src\hello.asm -l hello.dis
 cl65 -I ..\src -t apple2 -u __EXEHDR__ ..\src\hello.asm apple2.lib  -o hello.apple2 -C apple2-asm.cfg
 copy ..\disk\template.dsk hello.dsk
 java -jar C:\jar\AppleCommander.jar -p  hello.dsk hello.system sys < C:\cc65\target\apple2\util\loader.system
 java -jar C:\jar\AppleCommander.jar -as hello.dsk hello bin < hello.apple2 
-C:\AppleWin\Applewin.exe -no-printscreen-dlg -d1 hello.dsk 
+C:\AppleWin\Applewin.exe -no-printscreen-dlg -d1 hello.dsk
+copy hello.dsk ..\disk
+
 
